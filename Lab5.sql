39007dc5d0f5c1b418d73817018da464f7466d11
Pavel Knyazev <pasha2.05.97@mail.ru>
2019-12-15 21:02:05 +0300

    AddLeaf RemoveLeaf done + tests

--- Lab5.sql
+++ Lab5.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE TABLE nested(
     node_id INT NOT NULL PRIMARY KEY AUTOINC,
     node_name VARCHAR(16) NOT NULL UNIQUE,
-    left_num INT NOT NULL UNIQUE check(left_num > 0),
-    right_num INT NOT NULL UNIQUE check (right_num > 0),
+    left_num INT NOT NULL check(left_num > 0),
+    right_num INT NOT NULL check (right_num > 0),
     check(left_num < right_num)
 );
 
@@ -22,10 +22,12 @@ CREATE OR REPLACE PROCEDURE CountOfChildren(
 DECLARE
   VAR left_num, right_num, diff INT;
 CODE 
-  EXECUTE "SELECT left_num, right_num FROM nested" INTO left_num, right_num;
-  if left_num=NULL or right_num=NULL then
+  EXECUTE "SELECT left_num, right_num FROM nested WHERE node_id=?",
+    node_id INTO left_num, right_num;
+  
+  IF left_num=NULL OR right_num=NULL THEN
     RETURN -1;
-  endif;
+  ENDIF;
   
   RETURN (right_num - left_num + 1) / 2 - 1;
 END;
@@ -58,15 +60,31 @@ CODE
 
   parent_node_id := NameToId(parent_node_name);
   EXECUTE "SELECT right_num FROM nested WHERE node_id=?", parent_node_id INTO parent_right_num;
-  EXECUTE "UPDATE nested SET left_num=left_num+2 WHERE left_num>?",  parent_right_num;
-  EXECUTE "UPDATE nested SET right_num=right_num+2 WHERE right_num>?",  parent_right_num;
+  EXECUTE "UPDATE nested SET left_num=left_num+2 WHERE left_num>=?", parent_right_num + 1;
+  EXECUTE "UPDATE nested SET right_num=right_num+2 WHERE right_num>=?", parent_right_num;
   
   EXECUTE "INSERT INTO nested(node_name, left_num, right_num) VALUES(?,?,?)", 
-    new_node_name, parent_right_num + 1, parent_right_num + 2;
+    new_node_name, parent_right_num, parent_right_num + 1;
 
-  return 1;
+  RETURN parent_node_id;
 
 END;
 
-call AddLeaf('A');
-call AddLeaf('B', 'A');
+CREATE OR REPLACE PROCEDURE RemoveLeaf(
+  IN node_name varchar(16);
+) RESULT INT
+DECLARE
+  VAR node_id, right_num INT;
+CODE
+
+  node_id := NameToId(node_name);
+  IF CountOfChildren(node_id) <> 0 THEN
+    return -1;
+  ENDIF;
+
+  EXECUTE "SELECT right_num FROM nested WHERE node_id=?", node_id INTO right_num;
+  EXECUTE "UPDATE nested SET left_num=left_num-2 WHERE left_num>?", right_num - 1;
+  EXECUTE "UPDATE nested SET right_num=right_num-2 WHERE right_num>?", right_num;
+  EXECUTE "DELETE FROM nested WHERE node_id=?", node_id;
+  RETURN node_id;
+END;
